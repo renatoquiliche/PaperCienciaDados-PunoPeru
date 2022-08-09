ff68f752df3098c177222acc9abd5c90c447367e
renatoquiliche <renato.quiliche@pucp.pe>
2022-08-09 17:27:49 -0300

    Exp

--- Python Code/RFC.py
+++ Python Code/RFC.py
@@ -1,5 +1,5 @@
 """
-This script runs ENLR tunning for DS-PunoPeru
+This script runs RFC tunning for DS-PunoPeru
 
 @author: Renato Quiliche
 """
@@ -22,23 +22,14 @@ from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
 x[["altura", "gpc"]] = scaler.fit_transform(x[["altura", "gpc"]])
 
-# Get features format
-x.info()
-
-# Get balancing format
-from collections import Counter
-
-print(f"Original class counts: {Counter(Y)}")
-print("Classes are balanced")
-
 # Init the grid search cross-validation on ENLR
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 from sklearn.metrics import matthews_corrcoef, make_scorer, accuracy_score, f1_score, roc_curve
 #from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import RepeatedStratifiedKFold
 #from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.metrics import confusion_matrix
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 
 # I define here the NPV metric
 def neg_pred_value(y_true,y_predicted):
@@ -49,20 +40,20 @@ def neg_pred_value(y_true,y_predicted):
         neg_pred_value = cm[0][0]/(cm[1][0]+cm[0][0])
     return neg_pred_value
 
-"""
-Elastic net logistic regression
-"""
-
-def ENLRexperiments(K_folds, Repeats):
+def RFCexperiments(K_folds, Repeats):
     # Cross-validation method
     cv = RepeatedStratifiedKFold(n_splits=K_folds, n_repeats=Repeats, random_state=0)
     
     # Hyperparameter grid for ENLR
-    param_grid = { 'C': [1e-3, 1e-2, 1e-1, 1.0, 10.0, 100.0, 1000.0],
-                  'l1_ratio': np.round_(np.arange(0.0,1.1,0.1), decimals=1)}
+    param_grid = {'criterion': ['gini'],
+                  'max_depth': [2, 3, 4, 5, None],
+                  'min_samples_leaf': np.arange(0,1.1,0.1),
+                  'min_samples_split': np.arange(0,1.1,0.1),
+                  'n_estimators': [10, 100, 200, 500],
+                  'max_features': ['log2']}
     
     # I define the model here
-    ENLR = LogisticRegression(random_state=0, penalty='elasticnet', solver='saga', max_iter=4000, n_jobs=-1)
+    RFC = RandomForestClassifier(random_state=0, n_jobs=-1, bootstrap=True)
     scoring = {"AUC": "roc_auc"
                , "Accuracy": make_scorer(accuracy_score)
                , "F1-Score": "f1"
@@ -70,7 +61,8 @@ def ENLRexperiments(K_folds, Repeats):
                , "NPV": make_scorer(neg_pred_value)}
     
     #Test CV
-    search_ddnn= GridSearchCV(estimator = ENLR, param_grid=param_grid, scoring=scoring, cv=cv, n_jobs=-1, refit="MCC", verbose=0)
+    search_ddnn = RandomizedSearchCV(random_state=0, estimator = RFC, param_distributions=param_grid
+                                     , scoring=scoring, cv=cv, n_jobs=-1, refit="MCC", verbose=0, n_iter=200)
     results = search_ddnn.fit(x, Y)
     return results
 
@@ -78,20 +70,23 @@ def ENLRexperiments(K_folds, Repeats):
 Repeats = [15, 10, 8, 6]
 
 import time
-start = time.time()
 
-summary = {"C": [], "l1_ratio": [], "K_fold": [], "lambda": [], "composite": []}
+summary = {"param_criterion": [], "param_max_depth": [], "param_max_features": [], "param_min_samples_leaf": [],
+           "param_min_samples_split": [], "param_n_estimators": [], "K_fold": []}
 
 for i in range(2,6):
+    start = time.time()
     print("Configuration")
     print("K_folds =", i, "------- Repeats =", Repeats[i-2]*i)
-    exec(f'ENLRresults_{i} = ENLRexperiments({i}, Repeats[{(i-2)}])')
-    print("Elapsed Time ", (time.time() - start))
-    exec(f'print("Optimal Parameters :", ENLRresults_{i}.best_params_)')
-    exec(f'pd.DataFrame(ENLRresults_{i}.cv_results_).to_csv("Resultados/ENLR/ENLR_results_{i}.csv")')
+    exec(f'RFCResults_{i} = RFCexperiments({i}, Repeats[{(i-2)}])')
+    #print("Elapsed Time ", (time.time() - start))
+    exec(f'print("Optimal Parameters :", RFCResults_{i}.best_params_)')
+    exec(f'pd.DataFrame(RFCResults_{i}.cv_results_).to_csv("Resultados/RFC/RFC_results_{i}.csv")')
     #Construimos resumen de resultados
-    exec(f'summary["C"].append(ENLRresults_{i}.best_params_["C"])')
-    exec(f'summary["l1_ratio"].append(ENLRresults_{i}.best_params_["l1_ratio"])')
+    exec(f'summary["C"].append(RFCResults_{i}.best_params_["C"])')
+    exec(f'summary["l1_ratio"].append(RFCResults_{i}.best_params_["l1_ratio"])')
     exec(f'summary["K_fold"].append({i})')
     Total_time = (time.time() - start)
-    print(Total_time)
+    print("Iteration time: ", Total_time)
+
+pd.DataFrame(summary).to_excel("Resultados/RFC/summary.xlsx")
