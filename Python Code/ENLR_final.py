e30f087e50fc8f8f054f90f793b5acdf3fa2966f
renatoquiliche <renato.quiliche@pucp.pe>
2023-01-09 01:34:32 -0500

    Date

--- Python Code/ENLR_final.py
+++ Python Code/ENLR_final.py
@@ -9,7 +9,6 @@ from functools import partial
 from multiprocessing.connection import Pipe
 import pandas as pd
 import numpy as np
-import os
 
 # %%
 #Read the data from GitHub directly
@@ -99,9 +98,6 @@ print("Training time: ", Total_time, " seconds")
 pd.DataFrame(ENLR_results.cv_results_).to_csv("..\Resultados\ENLR\ENLR_results.csv")
 
 # %%
-results_dataset = pd.read_csv("..\Resultados\ENLR\ENLR_results.csv")
-
-# %%
 cutoff = round(n_iter*0.05)
 
 results_dataset = pd.DataFrame(ENLR_results.cv_results_)
@@ -119,9 +115,13 @@ print("Change in MCC: ", 100*(step5["mean_test_MCC"].iloc[0]-step4["mean_test_MC
 print("Change in NPV: ", 100*(step5["mean_test_NPV"].iloc[0]-step4["mean_test_NPV"].iloc[0])/(step4["mean_test_NPV"].iloc[0]))
 print("Percentil 5 cutoff: ", cutoff)
 
+print("AUC", "\t\t Accuracy", "\t\t F1-Score")
+print(step4["mean_test_AUC"].iloc[0], step4["mean_test_Accuracy"].iloc[0], step4["mean_test_F1-Score"].iloc[0])
+print(step5["mean_test_AUC"].iloc[0], step5["mean_test_Accuracy"].iloc[0], step5["mean_test_F1-Score"].iloc[0])
+
 # %%
-display(step4[["rank_test_MCC", "rank_test_NPV"]])
-display(step5[["rank_test_MCC", "rank_test_NPV"]])
+display(step4[["rank_test_MCC", "rank_test_NPV"]].head(10))
+display(step5[["rank_test_MCC", "rank_test_NPV"]].head(10))
 
 print("Parameters before NPV maximization: ")
 display(step4["params"].iloc[0])
@@ -137,7 +137,7 @@ for i in range(10):
     NPV_repeats["Repeat1"].append(f'split{i}_test_NPV')
 
 for i in range(10,20):
-    NPV_repeats["Repeat2"].append(f'split{i}_test_NPV')    
+    NPV_repeats["Repeat2"].append(f'split{i}_test_NPV')
 
 NPV = pd.DataFrame()
 
@@ -156,18 +156,28 @@ sns.boxplot(data=NPV_repeats, y="NPV", x="Repeat")
 
 # %%
 import seaborn as sns
-import pandas as pd
 
-results_dataset = pd.read_csv("..\Resultados\ENLR\ENLR_results.csv")
+MCC_repeats = {"Repeat1": [], "Repeat2": []}
+for i in range(10):
+    MCC_repeats["Repeat1"].append(f'split{i}_test_MCC')
 
-color_dict = dict({0.01:'brown',
-                  0.1:'green',
-                  1.0: 'orange',
-                  10.0: 'red',
-                   100.0: 'dodgerblue'})
+for i in range(10,20):
+    MCC_repeats["Repeat2"].append(f'split{i}_test_MCC')    
 
-sns.scatterplot(data=results_dataset, x="param_l1_ratio", y="mean_test_MCC", hue="param_C"
-                ,palette=color_dict)
+MCC = pd.DataFrame()
+
+MCC_repeats1 = pd.concat([step5.iloc[0:1].T.loc[MCC_repeats["Repeat1"]].reset_index(), 
+            pd.Series(np.ones(10).astype("int"))], axis=1, ignore_index=True)
+
+MCC_repeats2 = pd.concat([step5.iloc[0:1].T.loc[MCC_repeats["Repeat2"]].reset_index(), 
+            pd.Series(np.ones(10).astype("int"))+1], axis=1, ignore_index=True)            
+
+
+MCC_repeats = pd.concat([MCC_repeats1, MCC_repeats2], axis=0)
+MCC_repeats.columns = ["experiment", "MCC", "Repeat"]
+
+sns.set_theme()
+sns.boxplot(data=MCC_repeats, y="MCC", x="Repeat")
 # %%
 Coefficients = pd.DataFrame(columns=["Variable", "Coefficient"])
 
@@ -176,31 +186,31 @@ Coefficients["Coefficient"] = pd.DataFrame(ENLR_results.best_estimator_.coef_.T)
 Coefficients["Variable"] = pd.DataFrame(ENLR_results.feature_names_in_)
 
 # %%
-from sklearn.inspection import partial_dependence, plot_partial_dependence
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.scatterplot(data=results_dataset, x="param_l1_ratio", 
+              y="mean_test_MCC", hue="param_C"
+                , palette="tab10")
 
-## This function accepts one feature per evaluation, it maps the effects
-
-partial_dependence(ENLR_results.best_estimator_, X=x, features=72)
-plot_partial_dependence(ENLR_results.best_estimator_, X=x, features=[71])
-
+plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 # %%
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import ConfusionMatrixDisplay
-from sklearn.linear_model import LogisticRegression
+sns.scatterplot(data=results_dataset, x="param_l1_ratio", 
+              y="mean_test_NPV", hue="param_C"
+                , palette="tab10")
 
+plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+# %%
 params = step5["params"].iloc[0]
 
 import ast
-params = ast.literal_eval(params)
+#params = ast.literal_eval(params)
 
-winner_model = LogisticRegression(C=params['C'], l1_ratio=params['l1_ratio'],
-                                  penalty='elasticnet', solver='saga', 
-                                  max_iter=4000, n_jobs=-1,
-                                     random_state=0)
-
+winner_model = LogisticRegression(C=params["C"], l1_ratio=params["l1_ratio"], 
+                                  max_iter=4000, n_jobs=-1, penalty="elasticnet",
+                                  solver="saga", random_state=0)
 # %%
-import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
 
 x_train, x_test, y_train, y_test = train_test_split(x, Y, random_state=0, test_size=0.2, stratify=Y)
 winner_model.fit(x_train, y_train)
@@ -214,4 +224,52 @@ disp.plot()
 print('Negative predictive value: ', neg_pred_value(y_test, predictions))
 print('Mathews Correlation Coefficient: ', matthews_corrcoef(y_test, predictions))
 
+
 # %%
+
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(x, Y, random_state=0, test_size=0.2, stratify=Y)
+winner_model.fit(x_train, y_train)
+feature_importances = pd.DataFrame({'Features': winner_model.feature_names_in_,
+                                    'Importances': np.abs(winner_model.coef_[0])})
+
+sorted_results = feature_importances.sort_values(by=['Importances'], ascending=False)[0:15]
+
+sorted_results["Features"] = ["Internet", "Household is totally owned",
+                              "Household is located in a rural area", 
+                              "Concrete roof", "Manure cooking", "The head is married",
+                              "Outside tracks are paved", "Concrete floor", "Outside paths",
+                              "Concrete walls", "The head is employed", "Electric iron",
+                              "Motorcycle", "No public good", "Ligthing pole"]
+
+sns.barplot(data=sorted_results, y='Features', x='Importances', palette='GnBu_r')
+
+# %%
+
+grid = np.arange(0.1,0.99,0.01)
+
+res = {"test_size": [], "MCC": [], "NPV": []}
+
+for i in grid:
+    x_train, x_test, y_train, y_test = train_test_split(x, Y, random_state=0, test_size=i, stratify=Y)
+    winner_model.fit(x_train, y_train)
+    predictions = winner_model.predict(x_test)
+    res["test_size"].append(i*100)
+    res["MCC"].append(matthews_corrcoef(y_test, predictions)*100)
+    res["NPV"].append(neg_pred_value(y_test, predictions)*100)
+    
+results = pd.DataFrame(res)
+
+# %%
+sns.set_theme()
+g = sns.jointplot(data=results, x="test_size", y="MCC", kind="kde", fill=True, joint_kws={'alpha': 0.7})
+g.plot_joint(sns.scatterplot, alpha=0.8)
+
+# %%
+
+g = sns.jointplot(data=results, x="test_size", y="NPV", kind="kde", fill=True, joint_kws={'alpha': 0.7})
+g.plot_joint(sns.scatterplot, alpha=0.8)
+# %%
+
